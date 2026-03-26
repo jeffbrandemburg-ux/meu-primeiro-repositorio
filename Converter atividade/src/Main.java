77ea56275f46ece9c7414e18f966b3f73ceb5abf
jeffbrandemburg-ux <jeffbrandemburg@gmail.com>
2026-03-26 19:54:42 -0300

    Add files via upload

---
+++ Converter atividade/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        String numero = "8";
+
+        System.out.println(numero);
+
+        int numeroint = Integer.parseInt("8");
+
+        int numerofinal = (numeroint + 10);
+
+        System.out.println(numerofinal);
+
+    }
+    }
